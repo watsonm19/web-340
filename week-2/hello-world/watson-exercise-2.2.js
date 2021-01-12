02d4d3ff7250f11c57a1b29889b4846779ba192d
Mark Watson <watsonm19@me.com>
2021-01-12 14:35:41 -0800

    exercise 2.2 - hello world with express

--- week-2/hello-world/watson-exercise-2.2.js
+++ week-2/hello-world/watson-exercise-2.2.js
@@ -4,18 +4,21 @@
 ; Author:       Professor Krasso
 ; Date:         12 January 2021
 ; Modified By:  Mark Watson
-; Description:  Creates a new server with express and listens on port 8080.
+; Description:  This application creates a new server with express and listens on port 8080.
 ;==================================================
 */
 
+// imports express and http modules
 const express = require('express');
 const http = require('http');
 const app = express();
 
+// handles incoming http requests
 app.use(function(req, res) {
   console.log(`In comes a request to: ${req.url}`);
 
   res.end('Hello World');
 })
 
+// creates and starts server listening on port 8080
 http.createServer(app).listen(8080);
