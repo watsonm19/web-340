4d898b2dbfaf42f7ed180eee2eda92250c4cb5cc
watsonm19 <watsonm19@me.com>
2021-02-07 20:53:45 -0800

    exercise 5.2 - ejs templates

---
+++ week-5/if-else-render/watson-exercise-5.2.js
@@ -0,0 +1,45 @@
+/*
+===============================================
+; Title: Exercise 5.2 - EJS Templates
+; Author: Professor Krasso
+; Date: 07 February 2021
+; Modified By: Mark Watson
+; Description: This demonstrates
+===============================================
+*/
+
+// imports required modules
+const express = require('express');
+const http = require('http');
+const path = require('path');
+// initializes the app
+const app = express();
+
+// the views are in the "views" directory
+app.set('views', path.resolve(__dirname, 'views'));
+// use the EJS view engine
+app.set('view engine', 'ejs');
+
+// array - list of people
+const people = [
+  'Darlington',
+  'Diego',
+  'Fanendo',
+  'Rodney'
+]
+
+/**
+ * returns the index.ejs page
+*/
+app.get('/', function(req, res) {
+  res.render('index', {
+    people: people
+  })
+});
+
+/**
+ * creates server listening on PORT 8080
+*/
+http.createServer(app).listen(8080, function() {
+  console.log('App listening on PORT 8080');
+});
