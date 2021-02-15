d1826c3a245cf3452da6dd15ee3ef2162565dc3b
watsonm19 <watsonm19@me.com>
2021-02-14 21:29:16 -0800

    ems - milestone 2: user interface

--- ems/app.js
+++ ems/app.js
@@ -22,8 +22,27 @@ app.use(logger('short'));
 
 app.get('/', function(req, res) {
   res.render('index', {
-    title: 'Home page'
+    title: 'Homepage'
   })
+});
+
+app.get('/employees-list', function(req, res) {
+  res.render('list', {
+    title: 'All Employees'
+  });
+});
+
+app.get('/employee/:id', function(req, res) {
+  id = req.params.id;
+  res.render('view', {
+    title: `Employee: ${id}`
+  });
+});
+
+app.get('/new', function(req, res) {
+  res.render('new', {
+    title: 'Add Employee'
+  });
 });
 
 http.createServer(app).listen(8080, function() {
